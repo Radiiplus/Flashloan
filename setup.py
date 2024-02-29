b99757a086b72243e421a6d750710908e5cbeff6
Solid Scripts <152736140+Radiiplus@users.noreply.github.com>
2024-02-29 02:46:42 -1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def run_scripts():
     subprocess.run(['python3', req_script_path])
 
     # Change permissions on run.sh in the same folder (without running it)
-    run_script_path = os.path.join(script_directory, 'run.sh')
+    run_script_path = os.path.join(script_directory, 'run')
     subprocess.run(['chmod', '+x', run_script_path])
 
 if __name__ == "__main__":
