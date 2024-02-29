358716ca4b3a46fb3c492c227191401b86998fff
Positive Vibes <152736140+Radiiplus@users.noreply.github.com>
2024-02-29 03:20:37 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+def run_scripts():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Run req.py in the 'modules' folder
+    req_script_path = os.path.join(script_directory, 'modules', 'req.py')
+    subprocess.run(['python3', req_script_path])
+
+    # Change permissions on run.sh in the same folder (without running it)
+    run_script_path = os.path.join(script_directory, 'run.sh')
+    subprocess.run(['chmod', '+x', run_script_path])
+
+if __name__ == "__main__":
+    run_scripts()
